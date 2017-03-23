2c8a41ba3421146f46645fd81fb42a2ac872647c
Nilesh Chaturvedi <n.chaturvedi3@gmail.com>
2017-03-24 00:35:28 +0530

    Added data

--- Models/autoencoder.py
+++ Models/autoencoder.py
@@ -3,8 +3,10 @@
 from keras.layers import Input, Dense
 from keras.models import Model
 from keras.datasets import mnist
-import numpy as np
+from sklearn.preprocessing import normalize
 import matplotlib.pyplot as plt
+import numpy as np
+import csv
 
 def load_data(filename):
 
@@ -18,58 +20,59 @@ def load_data(filename):
 		y_comp.append(i[5])
 	
 	return [x_comp, y_comp]
-
+'''
+Define Model
+'''
 encoding_dim = 10
-
 input_img = Input(shape = (3,))
-
 encoded = Dense(encoding_dim, activation = 'relu')(input_img)
 decoded = Dense(3, activation = 'sigmoid')(encoded)
-
 autoencoder = Model(input = input_img, output = decoded)
-
 encoder = Model(input = input_img, output = encoded)
-
 encoded_input = Input(shape = (encoding_dim,))
 decoded_layer = autoencoder.layers[-1]
-
 decoder = Model(input = encoded_input, output = decoded_layer(encoded_input))
-
 autoencoder.compile(optimizer = 'adadelta', loss = 'binary_crossentropy')
 
+data = load_data("track1489609280.csv")
+x = np.array(data[0], dtype = np.float)
+y = np.array(data[0], dtype = np.float)
+dataX = [[i] for i in x]
+dataY = [[i] for i in y]
 
-(x_train, _), (x_test, _) = mnist.load_data()
+print(dataX)
 
-x_train = x_train.astype('float32')/255 #normalization
-x_test = x_test.astype('float32')/255
-x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
-x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
 
-print (x_train.shape)
-print (x_test.shape)
+# x_train = x_train.astype('float32')/255 #normalization
+# x_test = x_test.astype('float32')/255
+# x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+# x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
 
-autoencoder.fit(x_train, x_train,
-                nb_epoch=50,
-                batch_size=256,
-                shuffle=True,
-                validation_data=(x_test, x_test))
-encoded_imgs = encoder.predict(x_test)
-decoded_imgs = decoder.predict(encoded_imgs)
+# print (x_train.shape)
+# print (x_test.shape)
 
-n = 10  # how many digits we will display
-plt.figure(figsize=(20, 4))
-for i in range(n):
-    # display original
-    ax = plt.subplot(2, n, i + 1)
-    plt.imshow(x_test[i].reshape(28, 28))
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
+# autoencoder.fit(x_train, x_train,
+#                 nb_epoch=50,
+#                 batch_size=256,
+#                 shuffle=True,
+#                 validation_data=(x_test, x_test))
+# encoded_imgs = encoder.predict(x_test)
+# decoded_imgs = decoder.predict(encoded_imgs)
 
-    # display reconstruction
-    ax = plt.subplot(2, n, i + 1 + n)
-    plt.imshow(decoded_imgs[i].reshape(28, 28))
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-plt.show()
+# n = 10  # how many digits we will display
+# plt.figure(figsize=(20, 4))
+# for i in range(n):
+#     # display original
+#     ax = plt.subplot(2, n, i + 1)
+#     plt.imshow(x_test[i].reshape(28, 28))
+#     plt.gray()
+#     ax.get_xaxis().set_visible(False)
+#     ax.get_yaxis().set_visible(False)
+
+#     # display reconstruction
+#     ax = plt.subplot(2, n, i + 1 + n)
+#     plt.imshow(decoded_imgs[i].reshape(28, 28))
+#     plt.gray()
+#     ax.get_xaxis().set_visible(False)
+#     ax.get_yaxis().set_visible(False)
+# plt.show()
